07270af4047a3d089765b77d06127b4b7aa0fc59
Thomas Moser <tommoser100@hotmail.com>
2017-05-18 03:15:36 -0500

    Charge 2/hr before bedtime

--- src/main/java/BabysitterExpenseCalculator.java
+++ src/main/java/BabysitterExpenseCalculator.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
 
@@ -6,15 +7,18 @@ import java.util.List;
  * Created by tmoser on 5/17/17.
  */
 public class BabysitterExpenseCalculator {
+    private static final int BEFORE_BEDTIME_HOURLY_RATE = 12;
     private final List<BabysittingExpenseCalculatorValidator> validators;
 
     public BabysitterExpenseCalculator() {
         this.validators = Arrays.asList(new BusinessHoursValidator(), new TimesInOrderValidator(), new MaximumWorkLengthValidator());
     }
 
-    public int calculateExpense(LocalDateTime startTime, LocalDateTime endTime) {
+    public int calculateExpense(LocalDateTime startTime, LocalDateTime endTime, LocalDateTime bedTime) {
         validators.forEach(validator -> validator.validateTimes(startTime, endTime));
-        return 0;
+
+        int beforeBedtimeExpense = (int)startTime.until(bedTime, ChronoUnit.HOURS) * BEFORE_BEDTIME_HOURLY_RATE;
+        return beforeBedtimeExpense;
     }
 }
 
--- src/test/java/BabysitterExpenseCalculatorTest.java
+++ src/test/java/BabysitterExpenseCalculatorTest.java
@@ -26,8 +26,9 @@ public class BabysitterExpenseCalculatorTest {
     public void calculateShouldGiveZeroWhenLessThanAnHourOfWork() throws Exception {
         LocalDateTime startTime = LocalDateTime.of(2000, 5, 20, 17, 0);
         LocalDateTime endTime = LocalDateTime.of(2000, 5, 20, 17, 59);
+        LocalDateTime bedTime = endTime;
 
-        int expense = calculator.calculateExpense(startTime, endTime);
+        int expense = calculator.calculateExpense(startTime, endTime, bedTime);
 
         assertEquals(0, expense);
     }
@@ -36,35 +37,54 @@ public class BabysitterExpenseCalculatorTest {
     public void shouldThrowExceptionWhenStartTimeOutsideOf5pmTo4Am() throws Exception {
         LocalDateTime startTime = LocalDateTime.of(2000,5,20,16,0);
         LocalDateTime endTime = LocalDateTime.of(2000,5,20,18,0);
+        LocalDateTime bedTime = endTime;
 
         exception.expect(IllegalArgumentException.class);
-        calculator.calculateExpense(startTime, endTime);
+        calculator.calculateExpense(startTime, endTime, bedTime);
     }
 
     @Test
     public void shouldThrowExceptionWhenEndTimeOutsideOf5pmTo4Am() throws Exception {
         LocalDateTime startTime = LocalDateTime.of(2000,5,20,18,0);
         LocalDateTime endTime = LocalDateTime.of(2000,5,20,4,1);
+        LocalDateTime bedTime = endTime;
 
         exception.expect(IllegalArgumentException.class);
-        calculator.calculateExpense(startTime, endTime);
+        calculator.calculateExpense(startTime, endTime, bedTime);
     }
 
     @Test
     public void shouldThrowExceptionWhenEndTimeIsBeforeStartTime() throws Exception {
         LocalDateTime startTime = LocalDateTime.of(2000,5,20,19,0);
         LocalDateTime endTime = LocalDateTime.of(2000,5,20,18,0);
+        LocalDateTime bedTime = endTime;
 
         exception.expect(IllegalArgumentException.class);
-        calculator.calculateExpense(startTime, endTime);
+        calculator.calculateExpense(startTime, endTime, bedTime);
     }
 
     @Test
     public void shouldThrowExceptionWhenTimeWorkedLongerThanElevenHours() throws Exception {
         LocalDateTime startTime = LocalDateTime.of(2000,5,20,17,0);
         LocalDateTime endTime = LocalDateTime.of(2000,5,21,18,0);
+        LocalDateTime bedTime = endTime;
 
         exception.expect(IllegalArgumentException.class);
-        calculator.calculateExpense(startTime, endTime);
+        calculator.calculateExpense(startTime, endTime, bedTime);
+    }
+
+    @Test
+    public void shouldMakeTwelveDollarsAnHourFromStartUntilBedtime() {
+        int startingHour = 17;
+        int endingHour = 23;
+        LocalDateTime startTime = LocalDateTime.of(2000, 5, 20, startingHour, 0);
+        LocalDateTime bedTime = LocalDateTime.of(2000, 5, 20, endingHour, 0);
+        LocalDateTime endTime = bedTime;
+
+        int expense = calculator.calculateExpense(startTime, endTime, bedTime);
+
+        int untilBedtimeRate = 12;
+        int expectedExpense = (endingHour - startingHour) * untilBedtimeRate;
+        assertEquals(expectedExpense, expense);
     }
 }
