cf68f41c04187bc9b13306f6fb851c406fadbee6
Thomas Moser <tommoser100@hotmail.com>
2017-05-17 22:45:46 -0500

    Added basic project structure with sample test

---
+++ src/main/java/BabysitterExpenseCalculator.java
@@ -0,0 +1,5 @@
+/**
+ * Created by tmoser on 5/17/17.
+ */
+public class BabysitterExpenseCalculator {
+}
---
+++ src/test/java/BabysitterExpenseCalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by tmoser on 5/17/17.
+ */
+public class BabysitterExpenseCalculatorTest {
+
+    @Test
+    public void calculatorExists() {
+        assertNotNull(new BabysitterExpenseCalculator());
+    }
+}
