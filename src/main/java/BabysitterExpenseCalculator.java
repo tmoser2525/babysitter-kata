69c04958fff83e160152afece10cb6100733d459
Thomas Moser <tommoser100@hotmail.com>
2017-05-18 00:18:05 -0500

    Throw exception when start time is before 5pm

--- src/main/java/BabysitterExpenseCalculator.java
+++ src/main/java/BabysitterExpenseCalculator.java
@@ -4,7 +4,13 @@ import java.time.LocalDateTime;
  * Created by tmoser on 5/17/17.
  */
 public class BabysitterExpenseCalculator {
+
+    private static final int START_TIME_CUTOFF = 17;
+
     public int calculateExpense(LocalDateTime startTime, LocalDateTime endTime) {
+        if (startTime.getHour() < START_TIME_CUTOFF) {
+            throw new IllegalArgumentException("Start time must be later than 5pm");
+        }
         return 0;
     }
 }
--- src/test/java/BabysitterExpenseCalculatorTest.java
+++ src/test/java/BabysitterExpenseCalculatorTest.java
@@ -1,4 +1,6 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.time.LocalDateTime;
 
@@ -8,6 +10,9 @@ import static org.junit.Assert.assertEquals;
  * Created by tmoser on 5/17/17.
  */
 public class BabysitterExpenseCalculatorTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     @Test
     public void calculateShouldGiveZeroWhenLessThanAnHourOfWork() {
@@ -19,5 +24,15 @@ public class BabysitterExpenseCalculatorTest {
         int expense = calculator.calculateExpense(startTime, endTime);
 
         assertEquals(0, expense);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenStartingBeforeFivePm() {
+        BabysitterExpenseCalculator calculator = new BabysitterExpenseCalculator();
+        LocalDateTime startTime = LocalDateTime.of(2000,5,20,16,0);
+        LocalDateTime endTime = LocalDateTime.of(2000,5,20,18,0);
+
+        exception.expect(IllegalArgumentException.class);
+        calculator.calculateExpense(startTime, endTime);
     }
 }
